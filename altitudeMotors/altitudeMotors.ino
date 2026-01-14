c656e210f95b5634d580000a1ae24d8c57e0c28e
Radin <radin44@protonmail.com>
2026-01-13 21:58:28 -0800

    updated code

--- altitudeMotors/altitudeMotors.ino
+++ altitudeMotors/altitudeMotors.ino
@@ -1,59 +1,95 @@
 #include <Arduino.h>
 
-// set motor driver pins of integer type
-const int M1_INA {2};
-const int M1_INB {4};
-const int M1_PWM {9};
-const int M1_EN {6};
-const int M2_INA {7};
-const int M2_INB {8};
-const int M2_PWM {10};
-const int M2_EN {12};
+// motor driver pins
+const int M1_INA{2};
+const int M1_INB{4};
+const int M1_PWM{9};
+const int M1_EN {6};   // EN/DIAG
 
+const int M2_INA{7};
+const int M2_INB{8};
+const int M2_PWM{10};
+const int M2_EN {12};  // EN/DIAG
+
+// joystick
 const int readStick{15};
-int stickValue{};
-int pwmValue{};
+const int center{512};
 const int deadZone{20};
+const int maxSpeed{140};
+
+int stickValue{0};
+int pwmValue{0};
+int delta{};
 
 
 void setup() {
     pinMode(M1_INA, OUTPUT);
     pinMode(M1_INB, OUTPUT);
     pinMode(M1_PWM, OUTPUT);
+
     pinMode(M2_INA, OUTPUT);
     pinMode(M2_INB, OUTPUT);
     pinMode(M2_PWM, OUTPUT);
-    pinMode(M1_EN, OUTPUT);
-    pinMode(M2_EN, OUTPUT);
-    digitalWrite(M1_EN, HIGH);
-    digitalWrite(M2_EN, HIGH);
 
+    // EN/DIAG pins: open-drain → pull-up
+    pinMode(M1_EN, INPUT_PULLUP);
+    pinMode(M2_EN, INPUT_PULLUP);
 }
 
 void loop() {
     stickValue = analogRead(readStick);
-    pwmValue = map(stickValue, 0, 1023, 0, 255);
+    delta = stickValue - center;
 
-    if (stickValue > 512 + deadZone) {
+    // stop
+    if (abs(delta) < deadZone) {
+        analogWrite(M1_PWM, 0);
+        analogWrite(M2_PWM, 0);
+
+        // coast to stop
+        digitalWrite(M1_INA, LOW);
+        digitalWrite(M1_INB, LOW);
+        digitalWrite(M2_INA, LOW);
+        digitalWrite(M2_INB, LOW);
+    }
+
+    // Forward
+    else if (delta > 0) {
+        pwmValue = map(delta, deadZone, 512, 0, 255);
+
+        if (pwmValue > maxSpeed) {
+            pwmValue = maxSpeed;
+        }
+
         digitalWrite(M1_INA, HIGH);
         digitalWrite(M1_INB, LOW);
         digitalWrite(M2_INA, HIGH);
         digitalWrite(M2_INB, LOW);
+
         analogWrite(M1_PWM, pwmValue);
         analogWrite(M2_PWM, pwmValue);
     }
 
-    else if (stickValue < 512 - deadZone) {
+    // Reverse
+    else {
+        pwmValue = map(-delta, deadZone, 512, 0, 255);
+
+        if (pwmValue > maxSpeed) {
+            pwmValue = maxSpeed;
+        }
+
         digitalWrite(M1_INA, LOW);
         digitalWrite(M1_INB, HIGH);
         digitalWrite(M2_INA, LOW);
         digitalWrite(M2_INB, HIGH);
+
         analogWrite(M1_PWM, pwmValue);
         analogWrite(M2_PWM, pwmValue);
     }
-    else {
+
+    // fault check
+    if (digitalRead(M1_EN) == LOW || digitalRead(M2_EN) == LOW) {
+
         analogWrite(M1_PWM, 0);
         analogWrite(M2_PWM, 0);
     }
-    
 }
