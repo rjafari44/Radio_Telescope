5fdda229b6db9791bbb7be51826f340f4e95eb00
Radin <radin44@protonmail.com>
2026-01-13 22:02:08 -0800

    removed unnecessary directories

--- altitudeMotors/include/myHeader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// set motor driver pins of integer type
-inline constexpr int m1_INA {2};
-inline constexpr int m1_INB {4};
-inline constexpr int m1_PWM {9};
-inline constexpr int m2_INA {7};
-inline constexpr int m2_INB {8};
-inline constexpr int m2_PWM {10};
