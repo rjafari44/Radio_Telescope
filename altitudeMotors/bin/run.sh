cdce7f0ac855176334e5816c4a585a4154fcc878
Radin <radin44@protonmail.com>
2026-01-13 22:10:20 -0800

    updated run.sh

---
+++ altitudeMotors/bin/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Ask the user for their arduino board
+read -p "Enter your Arduino Port (e.g., /dev/ttyACM0): " PORT
+
+arduino-cli compile --fqbn arduino:avr:uno .
+
+# Upload the code to the board
+arduino-cli upload -p $PORT --fqbn arduino:avr:uno .
